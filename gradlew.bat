eea38b1286801c05cbd6f76c5ae41066bff98bed
d1667018881 <d1667018881@github.com>
2026-08-23 17:35:51 +0800

    fix: auto-resolve build errors - fix JVM opts quoting in gradlew script

--- gradlew.bat
+++ gradlew.bat
@@ -6,7 +6,7 @@ set DIRNAME=%~dp0
 if "%DIRNAME%" == "" set DIRNAME=.
 set APP_BASE_NAME=%~n0
 set APP_HOME=%DIRNAME%
-set DEFAULT_JVM_OPTS="-Xmx64m" "-Xms64m"
+set DEFAULT_JVM_OPTS=-Xmx64m -Xms64m
 set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
 @rem Find java.exe
 if defined JAVA_HOME goto findJavaFromJavaHome
